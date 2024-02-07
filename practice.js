87e4cb930d9bebe4166ad68ac3e25057b89fcce1
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-07 16:48:39 +0600

    aldkj

---
+++ practice.js
@@ -0,0 +1,36 @@
+/* 
+1. arrow function
+2. template string
+3. forEach (filter, find, map)
+4. API: fetch
+5. DOM er set
+*/
+
+/* 
+TASKS
+1. object with 5 properties. string, boolean, number, function (method, access any object property and return it), array , object
+2. template string: access values from the object above
+    2.1: array third element
+    2.2: property of the object
+
+3. Arrow function
+    3.1: no parameter return 89
+    3.2: number as parameter and return it by dividing by 17
+    3.3: two parameter: add 7 to the first number and add 5 to the second number and then
+    add them and then if the result is even return true otherwise return false 
+    3.4: multiline arrow function: array of numbers as input parameter: sum of all numbers
+    and the square of the result
+
+4. map
+number of array er eaach number diveide by 7 and return the array
+
+5. difference between map, forEach, filter, find, (reduce)
+
+6. Challenging: declare an object. use destructuring to get an object by excluding one property
+
+7. JSON placeholder show photos
+8. dynamically display album
+9. use arrow Function and forEach
+
+10. audodb
+*/
